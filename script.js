5a75e6d4eb1e3dc82eba78b9afe6d82e72d7b30c
SOUMEN MONDAL <151416831+Soumen-JavaDev@users.noreply.github.com>
2025-05-09 23:02:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+
+let btn=document.querySelector("#btn");
+
+btn.addEventListener("click",()=>{
+	let text=document.querySelector("#text").value.trim();
+let delay = parseInt(document.querySelector("#delay").value.trim());
+	helper(delay,text);
+	
+})
+function masg(params,text) {
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			resolve(text);
+		},params)
+	})
+}
+async function helper(delay,text){
+	try{
+		let data=await masg(delay,text);
+		alert(data);
+	}catch(er){
+		alert("error",er);
+	}
+}
